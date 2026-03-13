5baa3af8b592e412a297c2dc6419bf2b9e557c01
changshinyu <s04240922@gmail.com>
2026-03-13 17:30:23 +0800

    Create SelectionSort.cpp

---
+++ sorting/SelectionSort.cpp
@@ -0,0 +1,13 @@
+#include "SortingUtils.h"
+
+void SelectionSort(int data[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+            if (data[j] < data[min])
+                min = j;
+        Swap(data[i], data[min]);
+    }
+}
