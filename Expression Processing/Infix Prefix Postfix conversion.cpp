9894bc93a55c678723d710ee5a5196f5b9b54a75
changshinyu <s04240922@gmail.com>
2026-03-13 22:08:14 +0800

    Update Infix Prefix Postfix conversion.cpp

--- Expression Processing/Infix Prefix Postfix conversion.cpp
+++ Expression Processing/Infix Prefix Postfix conversion.cpp
@@ -39,19 +39,141 @@ public:
     String^ pop(int flag) { /*...*/ }
 
     // 中序 → 後序
-    String^ Infix2Postfix(String^ infix) { /*...*/ return ""; }
+    String^ Infix2Postfix(String^ infix)
+    {
+	    String^ postfix = "";
+	    String^ s, ^ x, ^ y;
+	    // Reset stacks before starting
+	    top = -1;
+	    top_another = -1;
+
+	    push(2, "#");
+	    for (int i = 0; i < infix->Length; i++)
+	    {
+		    s = infix[i].ToString(); // Remove leading space
+
+		    if (IsOperand(s)) // If it IS an operand
+			    postfix += s;
+		    else if (s == ")")
+		    {
+			    while ((x = pop(2)) != "(")
+				    postfix += x;
+		    }
+		    else
+		    {
+			    while (p(s) <= q(y = pop(2)))
+				    postfix += y;
+			    push(2, y);
+			    push(2, s);
+		    }
+	    }
+
+	// Empty the stack
+	    s = pop(2);
+	    while (s != "#")
+	    {
+		    postfix += s;
+		    s = pop(2);
+	    }
+	    return postfix;
+    }
 
     // 中序 → 前序
     String^ Infix2Prefix(String^ infix) { /*...*/ return ""; }
 
     // 前序 → 中序
-    String^ Prefix2Infix(String^ prefix) { /*...*/ return ""; }
+    String^ Prefix2Infix(String^ prefix)
+    {
+	    int n = prefix->Length;
+	    String^ ch, ^ op1, ^ op2, ^ expr;
+
+	    // 初始化 stack (flag = 1)
+	    top = -1;
+
+	    for (int i = n - 1; i >= 0; i--)
+	    {
+		    ch = prefix[i].ToString();
+
+		    if (Char::IsLetterOrDigit(ch[0]))  // 運算元
+		    {
+			    push(1, ch);
+		    }
+		    else  // 運算子
+		    {
+			    op1 = pop(1);
+			    op2 = pop(1);
+			    expr = "(" + op1 + ch + op2 + ")";
+			    push(1, expr);
+		    }
+	    }
+
+	    return pop(1);
+    }
 
     // 前序 → 後序
-    String^ Prefix2Postfix(String^ prefix) { /*...*/ return ""; }
+    String^ Prefix2Postfix(String^ prefix)
+    {
+	    int n = prefix->Length;
+	    // Reset stacks
+	    top = -1;
+	    top_another = -1;
+    
+	    String^ s, ^ y, ^ x;
+	    push(1, "#");
+    
+	    for (int i = 0; i < n; i++) // Corrected boundary
+	    {
+	       s = prefix[i].ToString();
+	       if (IsOperand(s))
+	       {
+	            while (top > 0 && IsOperand(Stack[top]))
+	            {
+				    y = pop(1);
+				    x = pop(1);
+				    s = y + s + x;
+			    }
+			    push(1, s);
+		    }
+		    else 
+			    push(1, s);
+	    }
+    
+	    s = pop(1);
+	    // Only pop again if there's something to pop
+	    if (top >= 0)
+	       x = pop(1);
+    
+	    return s;
+    }
 
     // 後序 → 中序
-    String^ Postfix2Infix(String^ postfix) { /*...*/ return ""; }
+    String^ Postfix2Infix(String^ postfix)
+    {
+	    int n = postfix->Length;
+	    String^ s, ^ s1, ^ s2, ^ x, ^ y;
+	    for (int i = 1;i < n;i++)
+	    {
+		    s = postfix[i].ToString();
+		    if (IsOperand(s))
+		    {
+			    push(2, s);
+			    push(1, "#");
+		    }
+		    else
+		    {
+			    s1 = pop(1);
+			    s2 = pop(1);
+			    x = pop(2);
+			    y = pop(2);
+			    if (p(s) > p(s1) && x->Length > 1) x = "(" + x + ")";
+			    if (p(s) > p(s2) && y->Length > 1) y = "(" + y + ")";
+			    push(2, y + s + x);
+			    push(1, s);
+		    }
+	    }
+	    while (top != -1) s = pop(1);
+	    return pop(2);
+    }
 
 private:
     void buttonInfix2Postfix_Click(Object^ sender, EventArgs^ e) {
