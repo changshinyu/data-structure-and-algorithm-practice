8045185079309360a2e9d659e08bcc71980ce6eb
changshinyu <s04240922@gmail.com>
2026-03-13 17:34:23 +0800

    Update BubbleSort.cpp

--- sorting/BubbleSort.cpp
+++ sorting/BubbleSort.cpp
@@ -2,8 +2,8 @@
 
 void BubbleSort(int list[], int n)
 {
-    for (int i=n-1 ; i>0 ; i--)
-        for (int j=1; j<=i; j++)
-            if (list[j-1] > list[j])
+    for(int i=n-1 ; i>0 ; i--)
+        for(int j=1; j<=i; j++)
+            if(list[j-1] > list[j])
                 Swap(list[j-1], list[j]);
 }
