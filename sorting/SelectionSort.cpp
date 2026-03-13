5567b7eceb5a886487291a6ca6365727dc4c1efb
changshinyu <s04240922@gmail.com>
2026-03-13 17:34:10 +0800

    Update SelectionSort.cpp

--- sorting/SelectionSort.cpp
+++ sorting/SelectionSort.cpp
@@ -2,12 +2,12 @@
 
 void SelectionSort(int data[], int n)
 {
-    for (int i = 0; i < n; i++)
+    for(int i=0; i<n; i++)
     {
-        int min = i;
-        for (int j = i + 1; j < n; j++)
-            if (data[j] < data[min])
-                min = j;
+        int min=i;
+        for(int j=i+1; j<n; j++)
+            if(data[j] < data[min])
+                min=j;
         Swap(data[i], data[min]);
     }
 }
